13936f35443ff36cb61d1001135e63db45e03914
alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-10-29 01:10:42 -0300

    Conectando com o banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,30 +4,39 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
+
+    public  Connection connectDB() {
         Connection conn = null;
-        
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?useSSL=false", "root", "Teste123");
+            System.out.println("Conectou do banco de dados.");
+        } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Erro: Não encontrou o driver do BD.");
         }
         return conn;
     }
-    
+
+    public void desconectar(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Desconectou do banco de dados.");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu desconectar do BD.");
+        }
+    }
 }
